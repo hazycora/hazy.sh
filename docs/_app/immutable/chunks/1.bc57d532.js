1360b94900a9d16a9dde713f7e8663faaf5d0f5b
hazycora <hazysu@riseup.net>
2023-04-30 20:08:29 -0500

    build for gh pages

---
+++ docs/_app/immutable/chunks/1.bc57d532.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.7142ae84.js";export{t as component};
--- docs/_app/immutable/chunks/2.38eb4421.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.496980d3.js";export{t as component};
