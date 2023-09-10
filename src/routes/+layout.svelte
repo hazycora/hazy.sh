cac1408e3ae74db89078cfda046780ce71a02deb
hazycora <hazysu@riseup.net>
2023-09-10 15:02:37 -0500

    fix hash :target breaking during hydration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
+	import { onMount } from 'svelte'
 	import '../app.postcss'
+
+	onMount(() => {
+		// The :target selector breaks during hydration
+		// so I'm setting it back up again here!
+		const hash = window.location.hash
+		window.location.hash = ''
+		window.location.hash = hash
+	})
 </script>
 
 <svelte:head>
