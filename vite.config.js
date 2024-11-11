ed78210d1250644d36e4e2934737a271e54ea3f0
hazycora <git@hazy.gay>
2024-11-11 17:20:30 -0600

    update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import svelteSVG from 'vite-plugin-svelte-svg'
+import svelteSVG from '@hazycora/vite-plugin-svelte-svg'
 
 export default defineConfig({
 	plugins: [
