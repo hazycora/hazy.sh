08f5be0608044a3207b3ef19bfc85c144eda2566
hazycora <git@hazy.gay>
2025-11-12 21:55:17 -0600

    resume improvements

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
-	import MetaTags from '$lib/components/MetaTags.svelte'
+	import Meter from './Meter.svelte'
+
+	const skills = [
+		{ name: 'VCS/Git', value: 98 },
+		{ name: 'Svelte', value: 98 },
+		{ name: 'TypeScript', value: 96 },
+		{ name: 'HTML+CSS', value: 96 },
+		{ name: 'Docker', value: 94 },
+		{ name: 'Go', value: 92 },
+		{ name: 'SQL & Redis', value: 90 },
+		{ name: 'Linux', value: 90 },
+		{ name: 'Figma', value: 85 },
+		{ name: 'Vue', value: 78 },
+		{ name: 'WebGL', value: 74 },
+		{ name: 'React', value: 70 },
+		{ name: 'Ruby on Rails', value: 60 }
+	]
 </script>
 
 <svelte:head>
@@ -9,7 +25,7 @@
 <article>
 	<header>
 		<h1>Hazel Cora</h1>
-		<p>Software Developer - Frontend & Backend <i>(she/her)</i></p>
+		<p>Software Developer, Frontend & Backend ⸱ Chicago, IL <i>(she/her)</i></p>
 		<ul class="links">
 			<li>
 				<a href="mailto:hey@hazy.gay">hey@hazy.gay</a>
@@ -21,28 +37,22 @@
 				<a href="https://hazy.sh/resume">Web link</a>
 			</li>
 		</ul>
+		<section>
+			<p>
+				I'm a lifelong software developer primarily interested in web
+				technologies. I maintain cloud services used by thousands of developers
+				globally, and have worked collaboratively for years as part of a
+				collective with other developers.
+			</p>
+		</section>
 	</header>
-	<section>
-		<p>
-			I'm a lifelong software developer passionate in both frontend and backend,
-			UI and UX. I maintain cloud services used by thousands of developers
-			globally, and have worked collaboratively for years as part of a
-			collective with other developers. My main interest is web technologies,
-			and most of my work has been with Go, TypeScript, Svelte, and Vue.
-		</p>
-	</section>
-	<hr />
 	<section>
 		<h2>Experience</h2>
 		<p></p>
 		<h3>System administrator, fullstack development - Besties</h3>
-		<p>
-			<i
-				>2021 - Present ⸱ <a href="https://besties.house"
-					>https://besties.house</a
-				></i
-			>
-		</p>
+		<small>
+			2021 - Present ⸱ <a href="https://besties.house">https://besties.house</a>
+		</small>
 		<p>
 			A developer collective I founded, creating and maintaining cloud services
 			for developers. Led development on nearly all projects by the collective
@@ -50,50 +60,42 @@
 			servers.
 		</p>
 		<ul>
-			<li>Sole developer of website hosting service <i>pages.gay</i></li>
 			<li>
-				Created developer tooling for the collective to aid in the creation and
-				maintenance of projects, including CI workflows, custom Vite plugins, and TypeScript libraries
+				Maintaining Git repository hosting provider git.gay, used by thousands
+				of developers, handling upgrades and server migrations
 			</li>
 			<li>
-				Maintaining internal services including the organisation's Matrix & XMPP
-				servers for internal communication
+				Built and maintain pages.gay, git.gay's companion website hosting
+				service
 			</li>
 			<li>
-				Maintaining Git repository hosting provider <i>git.gay</i>, used by
-				thousands of developers, handling upgrades and server migrations
+				Created internal developer tooling (CI workflows, custom Vite plugins,
+				TypeScript libraries)
 			</li>
+			<li>Operate internal Matrix & XMPP servers for teams</li>
 			<li>
-				Made tooling to debug iOS Safari browser compatibility issues from
-				Windows and Linux devices
+				Created tooling to debug iOS Safari browser compatibility issues from
+				non-macOS systems
 			</li>
 		</ul>
 		<h3>Website design & development - Honeybank Studios</h3>
-		<p>
-			<i
-				>2022 - 2023 ⸱ <a href="https://honeybankstudios.com"
-					>https://honeybankstudios.com</a
-				></i
+		<small>
+			2022 - 2023 ⸱ <a href="https://honeybankstudios.com"
+				>https://honeybankstudios.com</a
 			>
-		</p>
-		<p>
-			Worked in a team as a frontend developer & designer. Made a new website
-			for the company and aided in a proposed redesign of their product
-			TweetShift, and created MVP for a new product for the company
-		</p>
+		</small>
 		<ul>
 			<li>
-				Learned WebGL, GLSL shader development to create stylish effects for the
-				company site
+				Designed and developed reworked company site, using WebGL with a GLSL
+				shader for visual effects
 			</li>
 			<li>
-				Worked alongside a UI designer using Figma to implement a redesigned
+				Collaborated with a UI designer using Figma to implement a redesigned
 				dashboard for TweetShift users
 			</li>
 			<li>
-				R&D, design for planned product Pendant, a music streaming service made
-				for small & independent musicians. Designed and developed UI, reached
-				out to musicians to understand their needs
+				Led design and prototyping for Pendant, a music streaming MVP, with
+				feedback from musicians
 			</li>
 			<li>
 				Used GitHub CI and Cloudflare to test changes and deploy staging
@@ -101,54 +103,65 @@
 			</li>
 		</ul>
 	</section>
-	<hr />
+	<section>
+		<h2>Skills</h2>
+		<ul class="skill-list">
+			{#each skills as skill}
+				<li>
+					<label>
+						{skill.name}
+						<Meter value={skill.value} min={0} max={100} />
+					</label>
+				</li>
+			{/each}
+		</ul>
+	</section>
 	<section>
 		<h2>Active Projects</h2>
-		<h3><a href="https://git.gay">git.gay</a></h3>
-		<p><i>2022 - Present</i></p>
+		<h3>git.gay</h3>
+		<small>2022 - Present</small>
+		<p>Queer-friendly Git hosting platform based on a custom Forgejo fork</p>
 		<ul>
-			<li>
-				Git repository hosting service, providing spaces for FOSS projects by
-				queer developers
-			</li>
-			<li>
-				Running a custom modified version of Forgejo, actively maintained and
-				updated to the latest version of the upstream source code
-			</li>
-			<li>
-				Communicating with upstream developers of Forgejo, contributing to the
-				project with our own changes
-			</li>
+			<li>Maintaining upstream sync of forked codebase</li>
+			<li>Communication with upstream developers, contributing patches</li>
 		</ul>
-		<h3><a href="https://pages.gay">pages.gay</a></h3>
-		<p><i>2023 - Present</i></p>
+		<h3>pages.gay</h3>
+		<small>2023 - Present</small>
+		<p>Web hosting platform counterpart to git.gay</p>
 		<ul>
+			<li>Backend initially written in TypeScript before being ported to Go</li>
 			<li>
-				Custom backend, initially written in TypeScript before being ported to
-				Go
+				Automates TLS certificate issuance for custom domains with Let's Encrypt
+				APIs
 			</li>
 			<li>
-				Website hosting companion-service to git.gay, providing developers and
-				their projects with website hosting
-			</li>
-			<li>
-				Issuing TLS certificates on-the-fly to provide HTTPS for domains using
-				Let's Encrypt's API including for user's own custom domains
-			</li>
-			<li>
-				Programmatically modifying DNS using Cloudflare's API to enable issuing
-				of wildcard TLS certificates
+				Programmatically modifying DNS with Cloudflare API to issue wildcard TLS
+				certificates
 			</li>
 		</ul>
 	</section>
 </article>
 
 <style lang="postcss">
+	:global(*, *::before, *::after) {
+		box-sizing: border-box;
+	}
+
 	:global(:root) {
+		--page-bg-clr: #0a0a1a;
+		--bg-clr: #fff;
+		--heading-clr: #111118;
+		--text-clr: #2e2e3b;
+		--elevated-clr: #c4c4ee;
+		--border-clr: #9292be;
 		--accent-clr: #4848c1;
-		--accent-clr-alt: #4848c1;
+		--accent-alt-clr: #4848c1;
+
+		--meter-bar-clr: var(--elevated-clr);
+		--meter-progress-clr: var(--accent-clr);
 		font-family: 'DM Sans', 'Inter', system-ui, sans-serif;
 		line-height: 1.4;
+		color: var(--text-clr);
 	}
 
 	a,
@@ -158,14 +171,43 @@
 
 	article {
 		width: 100%;
-		max-width: 90rch;
+		max-width: 85rch;
 		margin-inline: auto;
 		margin-block: 2rem;
+
+		display: grid;
+		grid-auto-flow: row;
+		gap: 0.5rem;
+
+		@media (min-width: 700px) {
+			grid-template-columns: 1fr 2fr;
+		}
+
+		> :where(header, section) {
+			border: 1px solid var(--border-clr);
+			padding: 0.75rem;
+			border-radius: 0.5rem;
+
+			> :first-child {
+				margin-block-start: 0;
+			}
+
+			> :last-child {
+				margin-block-end: 0;
+			}
+		}
 	}
 
 	article > header {
+		grid-column: 1 / -1;
+
+		p {
+			margin-block-end: 0rem;
+		}
+
 		.links {
 			padding: 0;
+			margin-block: 0rem;
 			margin-inline: 0;
 			list-style: none;
 			display: flex;
@@ -182,8 +224,13 @@
 		}
 	}
 
+	article > section:first-of-type {
+		grid-column: 1 / -1;
+	}
+
 	ul {
-		margin-block: 0.25rem;
+		break-inside: avoid;
+		margin-block: 0.5rem;
 		padding-inline-start: 1.5rem;
 		> li {
 			margin-block: 0.25rem;
@@ -193,8 +240,14 @@
 	h1,
 	h2,
 	h3 {
+		color: var(--heading-clr);
+		break-after: avoid;
 		margin-block: 0.25rem 0.25rem;
+		& :global(~ small) {
+			font-style: italic;
+		}
 	}
+
 	h3 {
 		margin-block-start: 0.5rem;
 	}
@@ -203,7 +256,27 @@
 		margin-block: 0.25rem;
 	}
 
-	hr {
-		margin-block: 0.5rem;
+	ul.skill-list {
+		list-style: none;
+		padding: 0;
+		@media (min-width: 700px) {
+			width: max-content;
+		}
+
+		li {
+			margin-block: 0.5rem;
+		}
+
+		label {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+			@media (min-width: 700px) {
+				grid-template-columns: 1fr 10rem;
+			}
+			gap: 1.5rem;
+			align-items: center;
+			font-size: 1rem;
+			font-weight: 500;
+		}
 	}
 </style>
---
+++ src/routes/resume/Meter.svelte
@@ -0,0 +1,51 @@
+<!-- The <meter> tag does not get printed (at least in Chrome)... -->
+<script lang="ts">
+	let {
+		value,
+		min,
+		max
+	}: {
+		value: number
+		min: number
+		max: number
+	} = $props()
+</script>
+
+<meter {value} {min} {max}></meter>
+
+<div class="meter">
+	<div
+		class="progress"
+		style:width="{((value - min) / (max - min)) * 100}%"
+	></div>
+</div>
+
+<style>
+	/* sr-only */
+	meter {
+		position: absolute;
+		width: 1px;
+		height: 1px;
+		padding: 0;
+		margin: -1px;
+		overflow: hidden;
+		clip: rect(0, 0, 0, 0);
+		white-space: nowrap;
+		border-width: 0;
+	}
+
+	.meter {
+		width: 100%;
+		height: 1rem;
+		background-color: var(--meter-bar-clr);
+		print-color-adjust: exact;
+		border: none;
+		border-radius: 99999rem;
+
+		& .progress {
+			height: 100%;
+			border-radius: 99999rem;
+			background: var(--meter-progress-clr);
+		}
+	}
+</style>
