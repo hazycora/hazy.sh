5a795490ef28a3fc09f4930a04e1a3cf81d7adf1
hazycora <git@hazy.gay>
2025-12-25 16:06:26 -0600

    add ansi stuff to homepage

---
+++ src/hooks.server.ts
@@ -0,0 +1,36 @@
+import type { Handle } from '@sveltejs/kit'
+
+const csi = `\x1b[`
+const osc = `\x1b]`
+const st = `\x1b\\`
+
+const home = `${csi}2J${csi}H`
+const bold = `${csi}1m`
+const reset = `${csi}0m`
+const hidden = `${csi}8m`
+
+// not sure that this is safe, but whatever, i'm only using it here and know its inputs
+function link(url: string, text: string) {
+	url = url.replace(/\p{Cc}/gu, '')
+	text = text.replace(/\p{Cc}/gu, '')
+	return `${osc}8;;${url}${st}${text}${osc}8;;${st}`
+}
+
+function hiddenAnsi(text: string) {
+	return `<!--\n${home}${text}${hidden}--><div hidden>meow${reset}\n`
+}
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const response = await resolve(event)
+
+	if (event.url.pathname == '/') {
+		let body = await response.text()
+		body += hiddenAnsi(
+			`\n${bold}Hazel Cora 💞${reset}\ndev @ ${link('https://besties.house', 'besties.house')} · ${link('https://social.besties.house/@h', '@h@besties.house')} · ${link('mailto:hey@hazy.gay', 'hey@hazy.gay')}\n\nsee ${link('https://hazy.gay', 'hazy.gay')} in a browser for more <3\n`
+		)
+
+		return new Response(body, response)
+	}
+
+	return response
+}
